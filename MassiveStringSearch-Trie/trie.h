d74b40927be58b0c9410197f073adbdf181b99b4
Hanzeil <tang5266868@gmail.com>
2015-09-23 15:58:50 +0800

    Revert "增加分支 “TrieMap” Trie图实现方式分支"
    This reverts commit 16b820ce56b57e0ebddb0fa20b775208f3c0f8f9.

--- MassiveStringSearch-Trie/trie.h
+++ MassiveStringSearch-Trie/trie.h
@@ -2,7 +2,6 @@
 #define __TRIE_H__
 #include<fstream>
 #include<string>
-#include<map>
 using namespace std;
 struct TrieNode {
 	TrieNode* l[128];
@@ -11,13 +10,6 @@ struct TrieNode {
 		fill(begin(l), end(l), nullptr);
 	};
 };
-const map<char, int> email_format = {
-	{'-',0},
-	{}
-};
-void init(TrieNode *root) {
-
-}
 bool lookup(TrieNode *root,const string &s);
 void add(TrieNode *root, const string &s);
 void trie_check(ifstream &fp_strpool,ifstream &fp_checkedstr,ofstream &fp_result);
